e28053d73bd2c45e9b207e0a951384dc24f9539b
sameeeena <ayeshabano289@gmail.com>
2026-02-09 02:39:17 +0500

    push

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Set the root directory to resolve workspace warnings about multiple lockfiles
-  turbopack: {
-    root: process.cwd(), // Use absolute path to resolve workspace warnings
-  },
+  // turbopack: {
+  //   root: process.cwd(), // Use absolute path to resolve workspace warnings
+  // },
 };
 
 export default nextConfig;
+
